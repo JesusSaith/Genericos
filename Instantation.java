8d322242d507c151543a5a907821d166bd3863ef
JesusSaith <73085405+JesusSaith@users.noreply.github.com>
2020-10-28 13:09:24 -0600

    Add files via upload

---
+++ Instantation.java
@@ -0,0 +1,7 @@
+public class Instantation {
+
+Box<Integer> integerBox1 = new Box<Integer>();
+Box<Integer> integerBox2 = new Box<>();
+OrderedPair<String, Integer> p1 = new OrderedPair<>("Even", 8);
+    
+}
